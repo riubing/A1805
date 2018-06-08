51da2227c08a2a57f62a796c68183e5f4b631966
riubing <1440489020@qq.com>
2018-06-08 16:30:05 +0800

    cfsadfae

---
+++ 07day/01-老王.py
@@ -0,0 +1,11 @@
+while True:
+    name = input("请输入姓名:")
+    if name == "花花":
+        print("花花自残了")
+    elif name == "一凡":
+        print("一凡很傻")
+    elif name == "凡凡":
+        print("我说李冰很帅")
+    else:
+        print("李冰就是很帅")
+
---
+++ 07day/02-w微信登录.py
@@ -0,0 +1,6 @@
+zhanghoa = int(input("请输入账号："))
+pwd = int(input("请输入密码："))
+if (zhanghao == 123) and (pwd == 123):
+    print("登录成功")
+else:
+    print("登录失败")
